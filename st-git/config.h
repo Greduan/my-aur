c60a083edd453080c9abd738752803b9f8b52018
Greduan <eduanlavaque@gmail.com>
2014-10-27 18:20:22 -0600

    Stuff all around, a lot related to fonts and such

--- st-git/config.h
+++ st-git/config.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char font[] = "fixed:pixelsize=10:antialias=false:autohint=false"; */
-static char font[] = "dejavu sans mono:pixelsize=12";
+/* static char font[] = "dejavu sans mono:pixelsize=12"; */
+static char font[] = "inconsolata:pixelsize=14";
 static int borderpx = 0;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
